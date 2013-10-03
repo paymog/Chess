9d5576ccab5c98aa921fc80ecde4dd258f10d169
Paymahn Moghadasian <paymahn1@gmail.com>
2013-10-02 23:46:33 -0700

    Forgot to commit. There's a chessboard!
    There's now a chessboard. The visual definition can be found in App.xaml

---
+++ Chess/Models/BoardLocation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess.Models
+{
+    public enum BoardLocationColour
+    {
+        Light,
+        Dark
+    }
+
+
+    class BoardLocation: INotifyPropertyChanged
+    {
+        private BoardLocationColour _colour;
+        public BoardLocationColour Colour
+        {
+            get
+            {
+                return _colour;
+            }
+            private set
+            {
+                this._colour = value;
+                //this.OnPropertyChanged("Colour");
+            }
+        }
+
+        public BoardLocation(BoardLocationColour color)
+        {
+            this.Colour = color;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
---
+++ Chess/ViewModels/ChessBoardViewModel.cs
@@ -0,0 +1,62 @@
+﻿using Chess.Models;
+using MVVMToolkit;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess.ViewModels
+{
+    class ChessBoardViewModel : BaseViewModel
+    {
+        private const int NUM_ROWS = 8;
+        public int NumRows
+        {
+            get { return NUM_ROWS; }
+        }
+
+        private const int NUM_COLUMNS = 10;
+        public int NumColumns
+        {
+            get { return NUM_COLUMNS; }
+        }
+
+        private ObservableCollection<BoardLocation> _locations;
+        public ObservableCollection<BoardLocation> Locations
+        {
+            get
+            {
+                //Create the board the first time it is "gotten"
+                if (_locations == null)
+                {
+                    _locations = new ObservableCollection<BoardLocation>();
+                    for (int i = 0; i < NumRows; i++)
+                    {
+                        for (int j = 0; j < NumColumns / 2; j++)
+                        {
+                            if (i % 2 == 0)
+                            {
+                                _locations.Add(new BoardLocation(BoardLocationColour.Light));
+                                _locations.Add(new BoardLocation(BoardLocationColour.Dark));
+                            }
+                            else
+                            {
+                                _locations.Add(new BoardLocation(BoardLocationColour.Dark));
+                                _locations.Add(new BoardLocation(BoardLocationColour.Light));
+                            }
+                        }
+                    }
+                }
+                return _locations;
+            }
+        }
+
+        public ChessBoardViewModel()
+        {
+
+        }
+
+    }
+}
